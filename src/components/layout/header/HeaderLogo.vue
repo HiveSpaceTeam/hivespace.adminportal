4f339fa3333c08cf221517e0a6a70ed5d6af4003
thangnv-0703 <sunshines0703@gmail.com>
2025-09-03 20:36:05 +0700

    feat: update logo assets in sidebar and header components
    - Added new light logo SVG file at `public/images/logo/logo-light.svg`.
    - Updated `AppSidebar.vue` to use the new light logo for the default state.
    - Updated `HeaderLogo.vue` to use the new light logo for the mobile view.

--- src/components/layout/header/HeaderLogo.vue
+++ src/components/layout/header/HeaderLogo.vue
@@ -1,6 +1,6 @@
 <template>
   <router-link to="/demo" class="lg:hidden">
-    <img class="dark:hidden" src="/images/logo/logo.svg" :alt="$t('common.menu')" />
+    <img class="dark:hidden" src="/images/logo/logo-light.svg" :alt="$t('common.menu')" />
     <img class="hidden dark:block" src="/images/logo/logo-dark.svg" :alt="$t('common.menu')" />
   </router-link>
 </template>
