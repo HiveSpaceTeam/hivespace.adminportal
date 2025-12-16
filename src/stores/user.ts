f9f282ed3015b806880ef956e61d07a399de43c0
thangnv-0703 <sunshines0703@gmail.com>
2025-12-16 20:17:31 +0700

    refactor: Simplify user settings update and clean up AdminDetailModal template

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -143,11 +143,6 @@ export const useUserStore = defineStore('user', () => {
   const updateCulture = async (cultureValue: number) => {
     const newSettings = { ...userSettings.value, culture: cultureValue }
     await updateUserSettings(newSettings)
-
-    // Sync with i18n locale using conversion method
-    const i18n = (await import('@/i18n')).default
-    const culture = numericToStringCulture(cultureValue)
-    i18n.global.locale.value = culture
   }
 
   /**
